e50eafad5fddb1ca1e5580c0ad3631ca6bbd9b3f
Shravu874 <shravanidhamunse004@gmail.com>
2024-03-30 12:00:42 +0530

    init commit

--- components/alert.php
+++ components/alert.php
@@ -1,1 +1,11 @@
 <?php
+function unique_id(){
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charsLength = strlen($chars);
+    $randomString = '';
+    for ($i=0; $i < 20; $i++) {
+        $randomString.=$chars[mt_rand(0, $charLength - 1)];
+    }
+    return $randomString;
+}
+?>
